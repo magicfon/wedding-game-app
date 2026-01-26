0da80e09fb2e6391c07b4361949e02c220970cd2
magicfon <magicfon@gmail.com>
2026-01-26 23:37:11 +0800

    節點最上層v2

--- src/app/lottery-machine-live/page.tsx
+++ src/app/lottery-machine-live/page.tsx
@@ -1585,7 +1585,7 @@ export default function LotteryMachineLivePage() {
           left: 0;
           width: 100%;
           height: 100%;
-          z-index: 5;
+          z-index: 100;
           pointer-events: none;
         }
 
